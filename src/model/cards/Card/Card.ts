83b46a93b0d956f0b536e777ed71f88da7cc8815
kurehajime <xiidec@gmail.com>
2025-09-06 22:00:50 +0900

    クラス統合

--- src/model/cards/Card/Card.ts
+++ src/model/cards/Card/Card.ts
@@ -1,5 +1,6 @@
 import type { CardId } from '../../types';
 import type { Mana } from '../../Mana';
+import { GameManager } from '../../GameManager';
 
 export abstract class Card {
   readonly id: CardId;
@@ -23,6 +24,27 @@ export abstract class Card {
     this.gainMana = args.gainMana;
     this.text = args.text;
     this.isFixed = args.isFixed;
+  }
+
+  // 追加で得るマナを計算する
+  extendGainMana(_gameState: GameManager): { green: number, red: number, blue: number } {
+    return { green: 0, red: 0, blue: 0 };
+  }
+  // 与えるダメージを計算する
+  damage(_gameState: GameManager): number[] {
+    return [0, 0, 0, 0];
+  }
+  // プレイ可能なカードを制限する
+  hookEnabledPlay(_gameState: GameManager, hands: CardId[]): CardId[] {
+    return hands;
+  }
+  // ダメージを受けたときに他者にダメージを与える
+  hookDamageCounter(_gameState: GameManager, _damage: number): number[] {
+    return [0, 0, 0, 0];
+  }
+  // ダメージをキャンセルする
+  hookDamageCancel(_gameState: GameManager, _damage: number): boolean {
+    return false;
   }
 }
 
