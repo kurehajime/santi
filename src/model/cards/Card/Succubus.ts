6206e016de67095ca12f74eff94b476e9aca187b
kurehajime <xiidec@gmail.com>
2025-09-07 11:04:08 +0900

    サキュバス

--- src/model/cards/Card/Succubus.ts
+++ src/model/cards/Card/Succubus.ts
@@ -1,6 +1,10 @@
 import { CARD_ID } from '../ids';
 import type { Mana } from '../../Mana';
 import { Card } from './Card';
+import { GameState } from '../../GameState';
+import { damageByColor } from './cardUtil';
+import { CardId } from '../../types';
+import { CARDS } from '../../cards';
 
 export class Succubus extends Card {
   constructor() {
@@ -12,6 +16,17 @@ export class Succubus extends Card {
       text: '赤プレイヤーに🔵×1ダメージ。赤プレイヤーは次も赤しか出せない。',
       isFixed: false,
     });
+  }
+  damage(_gs: GameState): [number, number, number, number] {
+    return damageByColor(_gs, 'blue', 'red');
+  }
+  hookEnabledPlay(_gs: GameState, hands: CardId[]): CardId[] {
+    hands = hands.filter((id) => {
+      const card = CARDS.find((c) => c.id === id);
+      if (!card) return false;
+      return card.color === 'red';
+    });
+    return hands;
   }
 }
 
