954c3a3663cc380f51b8631ac92c9b8434db7b72
kurehajime <xiidec@gmail.com>
2025-09-07 10:59:01 +0900

    仙人

--- src/model/cards/Card/Hermit.ts
+++ src/model/cards/Card/Hermit.ts
@@ -1,6 +1,8 @@
 import { CARD_ID } from '../ids';
 import type { Mana } from '../../Mana';
 import { Card } from './Card';
+import { GameState } from '../../GameState';
+import { getColor } from './cardUtil';
 
 export class Hermit extends Card {
   constructor() {
@@ -12,6 +14,19 @@ export class Hermit extends Card {
       text: '受けるダメージをすべての赤プレイヤーに転嫁',
       isFixed: false,
     });
+  }
+  hookDamageCounter(_gs: GameState, _damage: number): [number, number, number, number] {
+    const damages: [number, number, number, number] = [0, 0, 0, 0];
+    for (let i = 0; i < _gs.players.length; i++) {
+      const color = getColor(_gs.players[i].openCard);
+      if (color === 'red') {
+        damages[i] = _damage;
+      }
+    }
+    return damages;
+  }
+  hookDamageCancel(_gs: GameState, _damage: number): boolean {
+    return true;
   }
 }
 
