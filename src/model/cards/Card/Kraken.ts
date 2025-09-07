a9ca0dc9fff423bb5c4c9f7b28b47d5866b88acc
kurehajime <xiidec@gmail.com>
2025-09-07 11:02:25 +0900

    クラーケン

--- src/model/cards/Card/Kraken.ts
+++ src/model/cards/Card/Kraken.ts
@@ -1,6 +1,8 @@
 import { CARD_ID } from '../ids';
 import type { Mana } from '../../Mana';
 import { Card } from './Card';
+import { GameState } from '../../GameState';
+import { getColor } from './cardUtil';
 
 export class Kraken extends Card {
   constructor() {
@@ -12,6 +14,23 @@ export class Kraken extends Card {
       text: '赤プレイヤーに青プレイヤーの🔵の合計ダメージ',
       isFixed: false,
     });
+  }
+  damage(_gs: GameState): [number, number, number, number] {
+    const damages: [number, number, number, number] = [0, 0, 0, 0];
+    let blueMana = 0;
+    for (let i = 0; i < _gs.players.length; i++) {
+      const color = getColor(_gs.players[i].openCard);
+      if (color === 'blue') {
+        blueMana += _gs.players[i].mana.blue;
+      }
+    }
+    for (let i = 0; i < _gs.players.length; i++) {
+      const color = getColor(_gs.players[i].openCard);
+      if (color === 'red') {
+        damages[i] = blueMana;
+      }
+    }
+    return damages;
   }
 }
 
